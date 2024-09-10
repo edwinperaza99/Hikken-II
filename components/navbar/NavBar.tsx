66ce56e477664cc14dc5736cf795fc168b306c33
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-09-10 21:34:28 +0000

    initial setup

---
+++ components/navbar/NavBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <div className="flex items-center justify-between h-16 px-4 bg-primary shadow-sm">
+        <div className="flex items-center">
+            <h1 className="text-2xl font-bold text-primary-foreground">Hikken II</h1>
+        </div>
+        <div className="flex items-center gap-4">
+        </div>
+        </div>
+    );
+    }
