a3b5f511af1432389375984d0b533061a70d08c2
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-09-11 03:12:37 -0700

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <h1>Hikken II</h1>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-       
-      </footer>
-    </div>
-  );
+	return (
+		<div className="">
+			<main className="flex flex-col items-center justify-center min-h-screen">
+				<div className="text-center uppercase flex flex-col gap-2">
+					<h1>Hikken II</h1>
+					<p className="uppercase">Coming soon...</p>
+					<Button>START</Button>
+				</div>
+			</main>
+		</div>
+	);
 }
