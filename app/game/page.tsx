ffa81339a36bafc3987162da1a0d8ee342bb1d71
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-09-13 02:20:47 -0700

    initial setup

---
+++ app/game/page.tsx
@@ -0,0 +1,10 @@
+export default function Game() {
+	return (
+		<div className="">
+			<main className="flex flex-col items-center justify-center min-h-screen">
+				<h1>coming soon...</h1>
+				<canvas></canvas>
+			</main>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -8,7 +9,9 @@ export default function Home() {
 				<div className="text-center uppercase flex flex-col gap-2">
 					<h1>Hikken II</h1>
 					<p className="uppercase">Coming soon...</p>
-					<Button>START</Button>
+					<Button asChild>
+						<Link href="/game">START</Link>
+					</Button>
 				</div>
 			</main>
 		</div>
